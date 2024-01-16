1a0a665756e73ff20d652c1d2dae6f36368ec7ef
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-01-16 12:45:21 +0200

    First commit lets go

---
+++ src/main/java/main/App.java
@@ -0,0 +1,51 @@
+// Joona Niemenmaa 16/01/2024 App.java
+package main;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        Hedgehog hedgehog = new Hedgehog();
+        
+        boolean exit = false;
+        while (exit == false) {
+            System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+            if (scanner.hasNext()) {
+                String choice = scanner.nextLine();
+                switch (choice) {
+                    case "1":
+                        System.out.println("Mitä siili sanoo:");
+                        String input = scanner.nextLine();
+                        hedgehog.speak(input);
+                        break;
+                    case "2":
+                        hedgehog = new Hedgehog();
+                        System.out.println("Anna siilin nimi:");
+                        String inputName = scanner.nextLine();
+                        hedgehog.setName(inputName);
+                        System.out.println("Anna siilin ikä:");
+                        int age = Integer.parseInt(scanner.nextLine());
+                        hedgehog.setAge(age);
+                        break;
+                    case "3":
+                        System.out.println("Kuinka monta kierrosta?");
+                        int iterations = Integer.parseInt(scanner.nextLine());
+                        hedgehog.run(iterations);
+                        break;
+                    case "0":
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Syöte oli väärä"); 
+                        break;
+                }
+            }
+        }
+    }
+}
+// EOF
---
+++ src/main/java/main/Hedgehog.java
@@ -0,0 +1,47 @@
+// Joona Niemenmaa 16/01/2024 Hedgehog.java
+package main;
+
+public class Hedgehog 
+{
+    private String name;
+    private int age;
+
+    public Hedgehog() {
+        name = "Pikseli";
+        age = 5;
+    }
+    public void speak(String input) {
+        String printout = "";
+        switch (input) {
+            case "":
+                printout = name + ": Olen " + name + " ja ikäni on " + age + ", mutta antaisitko silti syötteen?";
+                System.out.println(printout);
+                break;
+        
+            default:
+                printout = name + ": " + input;
+                System.out.println(printout);
+                break;
+        }
+        
+    }
+    public void run(int iterations) {
+        String printout = name + " juoksee kovaa vauhtia!";
+        for (int i = 0; i < iterations; i++) {
+            System.out.println(printout);
+        }
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+}
+// EOF
